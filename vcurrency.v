73476382752108e80698aae57519fdbf0a40d9e2
arnavmehta1977@gmail.com <66114536+mehtaarn000@users.noreply.github.com>
2021-03-01 15:35:00 -0600

    Add descriptive comments

--- vcurrency.v
+++ vcurrency.v
@@ -2,6 +2,7 @@ module vcurrency
 import x.json2
 import net.http
 
+// Json object to structure data
 pub struct JSONObject {
 	pub: 
 		json map[string]json2.Any
@@ -9,6 +10,8 @@ pub struct JSONObject {
 		json_string string
 }
 
+// Request data from API
+// Return struct JSONObject
 pub fn get_data(options map[string]string, symbols []string) JSONObject {
 	url := build_query(options, symbols)
 	resp := http.get(url) or { panic("Connection to server failed.") }
